5ec49334503e572dc969526ac27020886b831975
Mikhail Arkhipov <zet.ten@mail.ru>
2022-08-16 08:53:49 +0300

    vue-carousel

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <carousel
+    :slides="slides"
+    controls
+    indicators
+    :interval="3000"
+    :width="1000"
+    :height="450"
+  ></carousel>
+</template>
+
+<script>
+import Carousel from "./components/carousel/Carousel.vue";
+
+export default {
+  components: { Carousel },
+  data: () => ({
+    slides: [
+      "https://picsum.photos/id/1032/1000/450",
+      "https://picsum.photos/id/1033/1000/450",
+      "https://picsum.photos/id/1035/1000/450",
+      "https://picsum.photos/id/1036/1000/450",
+      "https://picsum.photos/id/1037/1000/450",
+    ],
+  }),
+};
+</script>
+
+<style></style>
